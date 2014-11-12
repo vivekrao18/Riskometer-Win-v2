543cda31097c74f4b17aa8639337cb1cfab20439
vivekrao18 <vivekrao@uw.edu>
2014-11-11 19:02:44 -0800

    updated pathway finder with API call.

--- RoR-v2/Page2.xaml.cs
+++ RoR-v2/Page2.xaml.cs
@@ -7,6 +7,18 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Windows.Media;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
 
 namespace PhoneApp1
 {
@@ -37,6 +49,7 @@ namespace PhoneApp1
 
         private void comorbidities_Click(object sender, RoutedEventArgs e)
         {
+
             if (!comorbidityFlag)
             {
                 stackPanelComor.Visibility = System.Windows.Visibility.Visible;
@@ -48,7 +61,90 @@ namespace PhoneApp1
                 stackPanelComor.Visibility = System.Windows.Visibility.Collapsed;
                 comorbidityFlag = false;
             }
+
+            sendTestAPICall();
+                       
+            
         }
+
+        private void sendTestAPICall()
+        {
+            System.Uri myUri = new System.Uri("http://intervention.cloudapp.net:8080/PathwayFinderAPI-0.0.1-SNAPSHOT/getpathway");
+            HttpWebRequest myRequest = (HttpWebRequest)HttpWebRequest.Create(myUri);
+            myRequest.Method = "POST";
+            myRequest.ContentType = "application/x-www-form-urlencoded";
+            myRequest.BeginGetRequestStream(new AsyncCallback(GetRequestStreamCallback), myRequest);
+
+        }
+
+        void GetRequestStreamCallback(IAsyncResult callbackResult)
+        {
+            HttpWebRequest myRequest = (HttpWebRequest)callbackResult.AsyncState;
+            Stream postStream = myRequest.EndGetRequestStream(callbackResult);
+
+            // Create the post data
+            string postData = "[{\"DM\":\"AGE_0, FEMALE_1\", \"PR\":\"PR_0388,PR_0345\",\"DX\":\"DX_22,DX_0954\",\"DEL\":\"CM_RENLFAIL_1\"}]";
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            
+            // Add the post data to the web request
+            postStream.Write(byteArray, 0, byteArray.Length);
+            postStream.Dispose();
+
+            // Start the web request
+            myRequest.BeginGetResponse(new AsyncCallback(GetResponsetStreamCallback), myRequest);
+        }
+
+        void GetResponsetStreamCallback(IAsyncResult callbackResult)
+        {
+            Debug.WriteLine("hi");
+
+            HttpWebRequest request = (HttpWebRequest)callbackResult.AsyncState;
+            HttpWebResponse response = (HttpWebResponse)request.EndGetResponse(callbackResult);
+            using (StreamReader httpWebStreamReader = new StreamReader(response.GetResponseStream()))
+            {
+
+
+                string result = httpWebStreamReader.ReadToEnd();
+              
+                JArray results = (JArray)JArray.Parse(result);
+
+                CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    for (int i = 0; i < 15; i++)
+                    {
+                        Button temp = new Button();
+                        temp.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x44, 0xAE, 0xCF));
+                        temp.Content = results[i]["icd9Code"] + ":" + results[i]["wt"];
+                        //Debug.WriteLine(results[i]["icd9Code"] + ":" + results[i]["wt"]);
+                        stackPanelComor.Children.Add(temp);
+                    }
+                 
+                    //foreach (JObject singleEntry in results)
+                    //{
+                    //    TextBlock temp = new TextBlock();
+                    //    // temp.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x44, 0xAE, 0xCF));
+                    //    temp.Text = singleEntry["icd9Code"] + ":" + singleEntry["wt"];
+                    // //   Debug.WriteLine(singleEntry["icd9Code"] + ":" + singleEntry["wt"]);
+                    //    stackPanelComor.Children.Add(temp);
+                    //   // Button temp = new Button();
+                    //   //// temp.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x44, 0xAE, 0xCF));
+                    //   // temp.Content = singleEntry["icd9Code"] + ":" + singleEntry["wt"];
+                    //   // Debug.WriteLine(singleEntry["icd9Code"] + ":" + singleEntry["wt"]);
+                    //   // stackPanelComor.Children.Add(temp);
+
+                    //}
+                    //   testTextBox.Text = result;      
+                });
+
+                ///   Debug.WriteLine(result);
+                // testTextBox.Text = result;                
+            }
+
+        }
+
+
+            
 
         private void interventions_Click(object sender, RoutedEventArgs e)
         {
--- RoR-v2/ResultsPage.xaml.cs
+++ RoR-v2/ResultsPage.xaml.cs
@@ -13,6 +13,8 @@ using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Windows.Media;
+using System.Text;
+using System.Diagnostics;
 
 namespace PhoneApp1
 {
@@ -21,9 +23,9 @@ namespace PhoneApp1
         public ResultsPage()
         {
             InitializeComponent();
-           
+
         }
-                
+
         string age = String.Empty;
         string bp = String.Empty;
         string gender = String.Empty;
@@ -37,7 +39,7 @@ namespace PhoneApp1
         string lengthOfStay = String.Empty;
 
 
-       protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
             bp = NavigationContext.QueryString["bp"];
@@ -49,175 +51,155 @@ namespace PhoneApp1
             diabetes = NavigationContext.QueryString["diabetes"];
             bmi = NavigationContext.QueryString["BMI"];
             efv = NavigationContext.QueryString["efv"];
-            
+
             lengthOfStay = NavigationContext.QueryString["Stay"];
 
         }
-       public void ResultPageLoaded(object sender, RoutedEventArgs e)
+        public void ResultPageLoaded(object sender, RoutedEventArgs e)
         {
+            System.Uri myUri = new System.Uri("http://cds-raas-dev.cloudapp.net/RoR");
+            HttpWebRequest myRequest = (HttpWebRequest)HttpWebRequest.Create(myUri);
+            myRequest.Method = "POST";
+            myRequest.ContentType = "application/x-www-form-urlencoded";
+            myRequest.BeginGetRequestStream(new AsyncCallback(GetRequestStreamCallback), myRequest);
+        }
 
-            String url = "http://charuch-ror.cloudapp.net/jsontest?age=" + age + "&gender=" + gender + "&ethnicGroup=" + race +
+        void GetRequestStreamCallback(IAsyncResult callbackResult)
+        {
+            HttpWebRequest myRequest = (HttpWebRequest)callbackResult.AsyncState;
+            Stream postStream = myRequest.EndGetRequestStream(callbackResult);
+
+            // Create the post data
+            string postData = "age=" + age + "&gender=" + gender + "&ethnicGroup=" + race +
                          "&inpatientBMI=" + bmi + "&diabetes=" + diabetes + "&stroke=" + stroke + "&priorHospitalization=" + hospitalization +
-                         "&ejectionFraction=" + efv + "&bloodPressureCategory=" + bp + "&lengthOfStay=" + lengthOfStay.Replace(" ", String.Empty);
+                         "&ejectionFraction=" + efv + "&bloodPressureCategory=" + bp + "&lengthOfStay=" + lengthOfStay.Replace(" ", String.Empty) + "&returnType=JSON&modelID=TenB";
+            //"[{\"DM\":\"AGE_0, FEMALE_1\", \"PR\":\"PR_0388,PR_0345\",\"DX\":\"DX_22,DX_0954\",\"DEL\":\"CM_RENLFAIL_1\"}]";
 
-            var request = (HttpWebRequest)WebRequest.Create(
-           // new Uri("http://charuch-ror.cloudapp.net/RoR?age=74&gender=Female&ethnicGroup=Asian&inpatientBMI=19&diabetes=Yes&stroke=No&priorHospitalization=No&ejectionFraction=20&bloodPressureCategory=201-219%20/%20100-110"));
-            new Uri(url));
-              request.BeginGetResponse(r =>          {
-              var httpRequest = (HttpWebRequest)r.AsyncState;
-              var httpResponse = (HttpWebResponse)httpRequest.EndGetResponse(r);
-              
-              using (var reader = new StreamReader(httpResponse.GetResponseStream()))
-              {
-                  var response = reader.ReadToEnd();
-                  int lastIndex = response.LastIndexOf('}');
-                  string responseString = response.Replace("TRUE", string.Empty);
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
 
-                  JObject objJsonObject = JObject.Parse(responseString);
+            // Add the post data to the web request
+            postStream.Write(byteArray, 0, byteArray.Length);
+            postStream.Dispose();
 
-                  string probability = objJsonObject["probability"].ToString();
-                  JObject riskFactorsObject = (JObject)objJsonObject["riskFactors"];
-                  JArray riskFactorsArray = (JArray)riskFactorsObject["name"];
-                  String[] risksStringList = new String[3];
+            // Start the web request
+            myRequest.BeginGetResponse(new AsyncCallback(GetResponsetStreamCallback), myRequest);
+        }
 
-                  JObject riskContributorsObject = (JObject)objJsonObject["readmissionContributors"];
-                  JArray riskContributorsArray = (JArray)riskContributorsObject["name"];
-                  String[] riskContributorsList = new String[3];
+        void GetResponsetStreamCallback(IAsyncResult callbackResult)
+        {
+            Debug.WriteLine("hi");
 
+            HttpWebRequest request = (HttpWebRequest)callbackResult.AsyncState;
+            HttpWebResponse result = (HttpWebResponse)request.EndGetResponse(callbackResult);
 
-                  Deployment.Current.Dispatcher.BeginInvoke(new Action(() =>
-                  {
+            using (StreamReader httpWebStreamReader = new StreamReader(result.GetResponseStream()))
+            {
+                var response = httpWebStreamReader.ReadToEnd();
+                int lastIndex = response.LastIndexOf('}');
+                string responseString = response.Replace("TRUE", string.Empty);
 
+                JObject objJsonObject = JObject.Parse(responseString);
 
-                      TextBlock probabilityTextBlock = new TextBlock();
-                      //TextBlock probab = new TextBlock();
-                      Probability.FontSize = 26;
-                      probabilityTextBlock.Text = "Probability of readmission in 30 days is";
-                      Probability.Text = probabilityTextBlock.Text.ToString();
-                      Double probabilityInDouble = Double.Parse(probability);
-                      if (probabilityInDouble < 35)
-                      {
-                          Prob.Foreground = new SolidColorBrush(Colors.Green);
-                       //   graphProbability.Foreground = new SolidColorBrush(Colors.Green);
-                  }
-                      else if (probabilityInDouble < 65 && probabilityInDouble > 35)
-                      {
-                          Prob.Foreground = new SolidColorBrush(Colors.Yellow);
-                         // graphProbability.Foreground = new SolidColorBrush(Colors.Yellow);
-                      }
-                      else
-                      {
-                          Prob.Foreground = new SolidColorBrush(Colors.Red);
-                          //graphProbability.Foreground = new SolidColorBrush(Colors.Green);
-                      }
-                 Prob.Text = probability.ToString();
+                string probability = objJsonObject["probability"].ToString();
+                JObject riskFactorsObject = (JObject)objJsonObject["riskFactors"];
+                JArray riskFactorsArray = (JArray)riskFactorsObject["name"];
+                String[] risksStringList = new String[3];
 
-                      graphCanvas.Visibility = System.Windows.Visibility.Visible;
-                      disclaimer.Visibility = System.Windows.Visibility.Visible;
-                     
-                     // graphProbability.Text = probability.ToString();
-                      Canvas.SetLeft(graphStack,probabilityInDouble);
-                      
-                     //String data = "<html>"
-                     //     + "<head>"
-                     //     + "<script type='text/javascript' src='https://www.google.com/jsapi'></script>"
-                     //     + "<script type='text/javascript'>"
-                     //     + "google.load('visualization', '1', {packages:['gauge']});"
-                     //     + " google.setOnLoadCallback(drawChart);"
-                     //     + " function drawChart() {"
-                     //     + " var data = google.visualization.arrayToDataTable(["
-                     //     + " ['Label', 'Value'],"
-                     //     + " ['Probability', "
-                     //     + probabilityInDouble
-                     //     + "],"
-
-                     //     + " ]); var options = {"
-                     //     + " width: 200, height: 200,"
-                     //     + " redFrom: 75, redTo: 100,"
-                     //     + " yellowFrom:50, yellowTo: 75,"
-
-                     //     + " minorTicks: 5"
-                     //     + "};"
-                     //     + "var chart = new google.visualization.Gauge(document.getElementById('chart_div'));"
-                     //     + " chart.draw(data, options);" + "  }" + " </script>"
-                     //     + "</head>" + " <body>"
-                     //     + "   <div id='chart_div'></div>" + "  </body>"
-                     //     + "</html>";
-                     //string data = "http://chart.googleapis.com/chart?chs=750x400&cht=gom&chd=t:" + probabilityInDouble + "&chco=FF0000,FF8040,FFFF00,00FF00,00FFFF,0000FF,800080&chxt=x,y&chxl=0:|Probability|1:|low|high";
-                     //string data = "http://chart.googleapis.com/chart?chs=750x400&cht=gom&chd=t:"+ probabilityInDouble+ "&chco=00FF00,00FF00,00FF00,00FE00,00FF00,FFFF00,FFFF00,FFFF00,FF0000,FF0000&chxt=x,y&chxl=0:|Probability|1:|low|high";
-                      //  graph.NavigateToString(data);
-                        
-  //       webview.Navigate(new Uri(data));
-                           
-                      // webview.NavigateToString(data);
-                     
-                    // webview.NavigateToString("<html><head><script type='text/javascript'src='http://www.google.com/jsapi'></script><script type='text/javascript'>google.load('visualization', '1', {packages:['gauge']}); google.setOnLoadCallback(drawChart); function drawChart(){ var data = google.visualization.arrayToDataTable([ ['Label', 'Value'], ['Probability', 63.05], ]); var options = { width: 200, height: 200, redFrom: 75, redTo: 100, yellowFrom:50, yellowTo: 75, minorTicks: 5};var chart = new google.visualization.Gauge(document.getElementById('chart_div')); chart.draw(data, options);  } </script></head> <body>   <div id='chart_div'></div>  </body></html>");
-                   
-                   //   webview.NavigateToString("<html><head><script type=\"text/javascript\" language=\"javascript\">function callme(){var test = alert(\"hello\");}</script></head><body onload=callme();> </body></html>");
+                JObject riskContributorsObject = (JObject)objJsonObject["readmissionContributors"];
+                JArray riskContributorsArray = (JArray)riskContributorsObject["name"];
+                String[] riskContributorsList = new String[3];
 
 
-
-                      TextBlock riskFactorsHeading = new TextBlock();
-                      riskFactorsHeading.Text = "Why this score?";
-                      riskFactorsHeading.FontSize = 30;
-                      riskFactorsHeading.TextAlignment = TextAlignment.Left;
-                      riskFactorsHeading.Margin = new Thickness(10, 10, 5, 10);
-                      ContentPanel.Children.Add(riskFactorsHeading);
-
-                      int i = 0;
-                      foreach (var riskFactors in riskFactorsArray)
-                      {
-                          TextBlock riskFactorsTextBlock = new TextBlock();
-                          risksStringList[i] = riskFactors.ToString();
-                          riskFactorsTextBlock.Text = i + 1 + ") " + risksStringList[i].Replace("&lt", " < ");
-                          riskFactorsTextBlock.Margin = new Thickness(10, 0, 5, 5);
-                          riskFactorsTextBlock.FontSize = 25;
-                          riskFactorsTextBlock.TextAlignment = TextAlignment.Left;
-                          ContentPanel.Children.Add(riskFactorsTextBlock);
-                          i++;
-
-                      }
+                Deployment.Current.Dispatcher.BeginInvoke(new Action(() =>
+                {
 
 
-                      TextBlock spacing = new TextBlock();
-                      spacing.Text = "";
-                      spacing.FontSize = 10;
-                      ContentPanel.Children.Add(spacing);
+                    TextBlock probabilityTextBlock = new TextBlock();
+                    //TextBlock probab = new TextBlock();
+                    Probability.FontSize = 26;
+                    probabilityTextBlock.Text = "Probability of readmission in 30 days is";
+                    Probability.Text = probabilityTextBlock.Text.ToString();
+                    Double probabilityInDouble = Double.Parse(probability);
+                    if (probabilityInDouble < 35)
+                    {
+                        Prob.Foreground = new SolidColorBrush(Colors.Green);
+                        //   graphProbability.Foreground = new SolidColorBrush(Colors.Green);
+                    }
+                    else if (probabilityInDouble < 65 && probabilityInDouble > 35)
+                    {
+                        Prob.Foreground = new SolidColorBrush(Colors.Yellow);
+                        // graphProbability.Foreground = new SolidColorBrush(Colors.Yellow);
+                    }
+                    else
+                    {
+                        Prob.Foreground = new SolidColorBrush(Colors.Red);
+                        //graphProbability.Foreground = new SolidColorBrush(Colors.Green);
+                    }
+
+                    Prob.Text = probability.ToString();
+
+                    graphCanvas.Visibility = System.Windows.Visibility.Visible;
+                    disclaimer.Visibility = System.Windows.Visibility.Visible;
+
+                    // graphProbability.Text = probability.ToString();
+                    Canvas.SetLeft(graphStack, probabilityInDouble);
 
 
-                      TextBlock riskContributorsHeading = new TextBlock();
-                      riskContributorsHeading.Text = "Ask a Physcian about:";
-                      riskContributorsHeading.FontSize = 30;
-                      riskContributorsHeading.TextAlignment = TextAlignment.Left;
-                      riskContributorsHeading.Margin = new Thickness(10, 10, 5, 10);
-                      ContentPanel.Children.Add(riskContributorsHeading);
+                    TextBlock riskFactorsHeading = new TextBlock();
+                    riskFactorsHeading.Text = "Why this score?";
+                    riskFactorsHeading.FontSize = 30;
+                    riskFactorsHeading.TextAlignment = TextAlignment.Left;
+                    riskFactorsHeading.Margin = new Thickness(10, 10, 5, 10);
+                    ContentPanel.Children.Add(riskFactorsHeading);
 
-                      int j = 0;
-                      foreach (var riskContributors in riskContributorsArray)
-                      {
-                          TextBlock riskContributorsTextBlock = new TextBlock();
-                          riskContributorsList[j] = j + 1 + ") " + riskContributors.ToString();
-                          riskContributorsTextBlock.Text = riskContributorsList[j];
-                          riskContributorsTextBlock.Margin = new Thickness(10, 0, 5, 5);
-                          riskContributorsTextBlock.TextAlignment = TextAlignment.Left;
-                          riskContributorsTextBlock.FontSize = 25;
-                          ContentPanel.Children.Add(riskContributorsTextBlock);
-                          j++;
+                    int i = 0;
+                    foreach (var riskFactors in riskFactorsArray)
+                    {
+                        TextBlock riskFactorsTextBlock = new TextBlock();
+                        risksStringList[i] = riskFactors.ToString();
+                        riskFactorsTextBlock.Text = i + 1 + ") " + risksStringList[i].Replace("&lt", " < ");
+                        riskFactorsTextBlock.Margin = new Thickness(10, 0, 5, 5);
+                        riskFactorsTextBlock.FontSize = 25;
+                        riskFactorsTextBlock.TextAlignment = TextAlignment.Left;
+                        ContentPanel.Children.Add(riskFactorsTextBlock);
+                        i++;
 
-                      }
-                  }));
+                    }
 
 
-              }
-           
-          
-          }, request);
+                    TextBlock spacing = new TextBlock();
+                    spacing.Text = "";
+                    spacing.FontSize = 10;
+                    ContentPanel.Children.Add(spacing);
+
+
+                    TextBlock riskContributorsHeading = new TextBlock();
+                    riskContributorsHeading.Text = "Ask a Physcian about:";
+                    riskContributorsHeading.FontSize = 30;
+                    riskContributorsHeading.TextAlignment = TextAlignment.Left;
+                    riskContributorsHeading.Margin = new Thickness(10, 10, 5, 10);
+                    ContentPanel.Children.Add(riskContributorsHeading);
+
+                    int j = 0;
+                    foreach (var riskContributors in riskContributorsArray)
+                    {
+                        TextBlock riskContributorsTextBlock = new TextBlock();
+                        riskContributorsList[j] = j + 1 + ") " + riskContributors.ToString();
+                        riskContributorsTextBlock.Text = riskContributorsList[j];
+                        riskContributorsTextBlock.Margin = new Thickness(10, 0, 5, 5);
+                        riskContributorsTextBlock.TextAlignment = TextAlignment.Left;
+                        riskContributorsTextBlock.FontSize = 25;
+                        ContentPanel.Children.Add(riskContributorsTextBlock);
+                        j++;
+
+                    }
+                }));
             }
 
-       }
 
 
-
+        }
     }
+}
 
     
